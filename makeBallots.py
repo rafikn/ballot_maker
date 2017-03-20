f5b0e8aacb4977a8fa1b804f7fd15179fab0301d
rafikn <rafikn@secureme.co.nz>
2017-03-20 16:50:13 +1300

    added tie breaking

--- makeBallots.py
+++ makeBallots.py
@@ -24,13 +24,14 @@ class Form:
 		self.file = file
 		self.candidates = candidates
 		self.votes = {}
-		self.duplicates = Set()
+		self.duplicates = dict()
 		self.missing = Set()
 		self.test = test
 		self.stage = stage
 		self.ranked = 0
 		self.error = False
 		self.BLT = list()
+		self.gaps = 0
 
 	def toBLT(self):
 		"""
@@ -83,8 +84,35 @@ class Form:
 						if x <= NUM_CANDIDATES:
 							self.missing.add(x)
 
+
+	def updateDuplicates(self, rank, candidate):
+		try:
+			existing = self.duplicates[rank]
+		except KeyError, err:
+			existing = list()
+			existing.append(self.votes[rank])
+
+
+		if candidate not in existing:
+			existing.append(candidate)
+
+		self.duplicates[rank] = existing
+
+
+	def addVote(self, vote, candidate):
+		if (len(vote) > 0):
+			if int(vote) in self.votes.keys():
+				self.updateDuplicates(int(vote), candidate)
+
+			if int(vote) > NUM_CANDIDATES:
+				self.printOutOfBound(vote)
+
+			self.votes[int(vote)] = candidate
+			self.ranked += 1
+
+
 	def load(self):
-		self.duplicates = Set()
+		self.duplicates = dict()
 		""" 
 			Parse Voting from to BLT 
 		"""
@@ -102,29 +130,11 @@ class Form:
 
 				candidate_1 = line[0].lstrip();
 				vote_1 = line[1].lstrip();
+				self.addVote(vote_1, candidate_1)
+
 				candidate_2 = line[3].lstrip();
 				vote_2 = line[4].lstrip();
-
-				if (len(vote_1) > 0):
-
-					if int(vote_1) in self.votes.keys():
-						self.duplicates.add(int(vote_1))
-
-					if int(vote_1) > NUM_CANDIDATES:
-						self.printOutOfBound(vote_1)
-
-					self.votes[int(vote_1)] = candidate_1
-					self.ranked += 1
-
-				if (len(vote_2) > 0):
-					if int(vote_2) in self.votes.keys():
-						self.duplicates.add(int(vote_2))
-
-					if int(vote_2) > NUM_CANDIDATES:
-						self.printOutOfBound(vote_2)
-
-					self.votes[int(vote_2)] = candidate_2
-					self.ranked += 1
+				self.addVote(vote_2, candidate_2)
 
 
 			diff = list(keys.symmetric_difference(self.votes.keys()))
@@ -138,24 +148,58 @@ class Form:
 					self.BLT.append(self.candidates[candidate])
 				except KeyError, err:
 					print '[ERROR]: Cannot find index for candidate %s' % str(err)
+
+
+	def checkGaps(self):
+		orderedDuplicates= sorted(self.duplicates.keys())
+		orderedMissing = sorted(self.missing)
+
+		if (len(orderedDuplicates) > 0):
+			firstDuplicate = orderedDuplicates[0]
+			self.gaps = 0
+			for missing in orderedMissing:
+				if  missing < firstDuplicate:
+					""" move candidates one rank up"""
+					self.gaps += 1
+
+
+	def truncate(self):
+		orderedDuplicates= sorted(self.duplicates.keys())
+		orderedMissing = sorted(self.missing)
+
+		if (len(orderedDuplicates) > 0):
+			firstDuplicate = orderedDuplicates[0]
+			self.gaps = 0
+			for missing in orderedMissing:
+				if  missing < firstDuplicate:
+					""" move candidates one rank up"""
+					self.gaps += 1
+
+			self.BLT = self.BLT[:(firstDuplicate- self.gaps - 1)]
+			return self
+
+	def breakTies(self, list):
+		"""
+			Break ties according to an initial list ranking
+		"""
+
 					
 
 class BallotMaker:
 	""" 
 		Transoform a list ranking into a BLT format 
 	"""
-	candidates = {}
-	inverted = {}
-	ranking = {}
-	stage = "INIT"
 
-	forms = list()
 
 	def __init__(self, candidateFile, stage):
 		""" 
 			Load candidates from file into a dict 
 		"""
 		self.stage = stage
+		self.candidates = {}
+		self.inverted = {}
+		self.ranking = {}
+		self.forms = list()
 
 		with open(candidateFile, 'r') as csvfile:
 			candidateList = csv.reader(csvfile)
@@ -175,8 +219,6 @@ class BallotMaker:
 		""" 
 			Load voting forms from path
 		"""
-		self.forms = []
-
 		files = glob.glob(path + "/*.csv")
 		if (len(files) == 0):
 			print "No forms found in folder:", path
@@ -195,21 +237,7 @@ class BallotMaker:
 				form = Form(file, self.candidates, self.stage, test)
 				form.load()
 
-				truncated = self.truncate(form)
-
-				if (invert == False and not test and form.ranked >= 25):
-					""
-					print form.toBLT()
-
 				self.forms.append(form)
-
-	def truncate(self, form):
-		orderedDuplicates= sorted(form.duplicates)
-		orderedMissing = sorted(form.missing)
-
-		if (len(orderedDuplicates) > 0):
-			firstDuplicate = orderedDuplicates[0]
-			form.BLT = form.BLT[:(firstDuplicate-1)]
 
 
 
@@ -229,6 +257,7 @@ class BallotMaker:
 					try:
 						ranks.append(form.BLT.index(candidateIndex) + 1)
 					except ValueError, err:
+						ranks.append(-1)
 						print "[WARN] cadidate '%s' not rankend in '%s'" % (candidate, form.file)
 				writer.writerow(ranks)
 
@@ -261,7 +290,7 @@ if __name__ == '__main__':
 		print usage
 		sys.exit(1)
 
-	if len(opts) > 3 or len(opts) < 2:
+	if len(opts) > 4 or len(opts) < 2:
 		if len(opts) < 2:
 			print "Specify forms folder and candidates file"
 			print usage
@@ -273,6 +302,7 @@ if __name__ == '__main__':
 
 	formsPath = ''
 	candidateFile = ''
+	listPath= ''
 	test = False
 	invert = False
 
@@ -297,6 +327,10 @@ if __name__ == '__main__':
 			test = True
 		if o == "-i":
 			invert = True
+		if o == "-l":
+			listPath = a
+		if o == "-s":
+			stage = a
 
 	if (formsPath == ''):
 		print "Specify forms folder";
@@ -313,7 +347,108 @@ if __name__ == '__main__':
 	#print maker.candidates
 	maker.loadForms(formsPath, test)
 
+	if (stage == 'INIT'):
+		for form in maker.forms:
+			form.truncate()
+			print form.toBLT()
+	elif (stage == 'FIX'):
+		ranked = list()
+		with open(listPath, 'rU') as rankedCandidates:
+			candidates = csv.reader(rankedCandidates)
+			for candidate in candidates:
+				ranked.append(candidate[0])
+
+		for form in maker.forms:
+			if (form.ranked < 25):
+				continue
+
+			line = '1 '
+			for c in form.BLT:
+				candidate = form.candidates.keys()[form.candidates.values().index(c)]
+
+				duplicate = False
+				for tie in form.duplicates.values():
+					if candidate in tie:
+						duplicate = True
+
+						brokentie = dict()
+						for c in tie:
+							brokentie[ranked.index(c)] = c
+
+						for rank in sorted(brokentie.keys()):
+							candidateAtRank = brokentie[rank]
+							line += str(maker.candidates[candidateAtRank]) + ' '
+
+
+				if not duplicate:
+					line += str(c) + ' '
+
+				# line += str(rank) + ' ' 
+						
+			line += '0'
+			print line	
+
+			
+
+					# if (len(range(1, form.gaps + 1)) > 0):
+					# 	for gap in range(1, form.gaps + 1):
+					# 		adjusted = current + gap
+					# 		#dostuff
+
+					# 		if adjusted in form.duplicates.keys():
+					# 		#print 'found duplicates', current 
+					# 		#print 'candidates tied: ', form.duplicates[current]
+
+					# 			for c in form.duplicates[current]:
+					# 				brokentie[ranked.index(c) + 1] = c
+
+					# 			for rank in sorted(brokentie.keys()):
+					# 				candidateAtRank = brokentie[rank]
+					# 				print candidateAtRank, 'index', maker.candidates[candidateAtRank]
+
+					# 				print current, str(maker.candidates[candidateAtRank])
+
+					# 				line += str(maker.candidates[candidateAtRank]) + ' ' 
+					# 				current += 1
+					# 		else:
+					# 			print current, str(form.BLT[current - 1])
+					# 			line += str(form.BLT[current - 1]) + ' ' 
+
+					# 		form.gaps -= 1
+
+					# else:
+					# 	#dostuff
+					# 	if adjusted in form.duplicates.keys():
+					# 		#print 'found duplicates', current 
+					# 		#print 'candidates tied: ', form.duplicates[current]
+
+					# 		for c in form.duplicates[current]:
+					# 			brokentie[ranked.index(c) + 1] = c
+
+					# 		for rank in sorted(brokentie.keys()):
+					# 			candidateAtRank = brokentie[rank]
+					# 			print candidateAtRank, 'index', maker.candidates[candidateAtRank]
+
+					# 			print current, str(maker.candidates[candidateAtRank])
+
+					# 			line += str(maker.candidates[candidateAtRank]) + ' ' 
+					# 			current += 1
+					# 	else:
+					# 		print current, str(form.BLT[current - 1])
+					# 		line += str(form.BLT[current - 1]) + ' ' 
+
+
+
+
+
+				
+										
+
+
+					
+					# else:
+					# 	line += str(rank) + ' ' 
+
+
 	if (invert):
 		maker.invert()
-
-
